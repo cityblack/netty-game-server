f5638864b8cbfa742378afefcf7d1ce3661309b2
lzh <as123456>
2020-01-10 18:00:37 +0800

    Delete test code

--- game-repository/src/test/java/com/lzh/game/repository/repository/ItemBoxRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lzh.game.repository.repository;
-
-import com.lzh.game.repository.entity.PlayerItemBox;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface ItemBoxRepository extends PagingAndSortingRepository<PlayerItemBox, String> {
-
-}
