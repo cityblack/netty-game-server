9d344d46cf7727b2b6312255a69db0e8d6beaeb0
lzh <867708644@qq.com>
2022-03-29 11:37:47 +0800

    Change method invoke utils

--- game-common/src/main/java/com/lzh/game/common/bean/MethodInvokeUtils.java
+++ game-common/src/main/java/com/lzh/game/common/bean/MethodInvokeUtils.java
@@ -1,19 +1,19 @@
 package com.lzh.game.common.bean;
 
 import javassist.*;
+import javassist.bytecode.ClassFile;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 
 import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
 public class MethodInvokeUtils {
 
     private static ClassPool pl;
-
-    private static String OBJECT_ARR_STR = "([Ljava/lang/Object;)Ljava/lang/Object;";
 
     /**
      * Use Javassist to enhance Java method reflect
@@ -26,6 +26,7 @@ public class MethodInvokeUtils {
 
         String superName = bean.getClass().getName();
         String newClassName = superName.replaceAll("(.+)\\.(\\w+)", "$1.MethodInvoke$2") + "_" + method.getName();
+        pl.appendClassPath(new ClassClassPath(bean.getClass()));
 
         CtClass enhance = pl.makeClass(newClassName);
         enhance.setInterfaces(new CtClass[]{pl.getCtClass(MethodInvoke.class.getName())});
@@ -39,13 +40,11 @@ public class MethodInvokeUtils {
         constructor.setBody("{this.bean = $1;}");
         enhance.addConstructor(constructor);
 
-        String methodName = "invoke";
-        CtClass methodClass = pl.getCtClass(MethodInvoke.class.getName());
-        CtMethod ctMethod = methodClass.getMethod(methodName, OBJECT_ARR_STR);
-        CtMethod invoke = new CtMethod(ctMethod.getReturnType(), methodName, ctMethod.getParameterTypes(), enhance);
-        String methodBody = buildInvokeMethodBody(method);
-        invoke.setBody(methodBody);
+        CtMethod invoke = CtNewMethod.make("public Object invoke(Object[] args) { " + buildInvokeMethodBody(method) + " }", enhance);
         enhance.addMethod(invoke);
+
+        enhance.getClassFile2().setMinorVersion(ClassFile.JAVA_8);
+        enhance.writeFile(MethodInvoke.class.getClassLoader().getResource("./").getFile());
         Constructor<?> enhanceConstructor = enhance.toClass().getConstructor(bean.getClass());
         return (MethodInvoke) BeanUtils.instantiateClass(enhanceConstructor, bean);
     }
@@ -55,20 +54,40 @@ public class MethodInvokeUtils {
         Class<?> returnType = method.getReturnType();
         StringBuilder builder = new StringBuilder();
         builder.append("{");
-        if (returnType != Void.class) {
+        String methodName = method.getName();
+        if (returnType != Void.class && returnType != void.class) {
             builder.append("return ");
+            addBeanInvokeToBody(builder, types, methodName);
+        } else {
+            addBeanInvokeToBody(builder, types, methodName);
+            builder.append("return null;");
         }
-        builder.append("this.bean.").append(method.getName()).append("(");
+        builder.append("}");
+        return builder.toString();
+    }
+
+    private static void addBeanInvokeToBody(StringBuilder builder, Class<?>[] types, String methodName) {
+        builder.append("this.bean.").append(methodName).append("(");
         for (int i = 0; i < types.length; i++) {
             Class<?> type = types[i];
-            builder.append("(").append(type.getName()).append(")")
-                    .append("$1[").append(i).append("]");
+            checkParamType(type);
+            builder.append("(")
+                    .append(type.getName())
+                    .append(")")
+                    .append("args[").append(i).append("]");
             if (i != types.length - 1) {
                 builder.append(",");
             }
         }
-        builder.append(");}");
-        return builder.toString();
+        builder.append(");");
+    }
+
+    // no support base type
+    private static void checkParamType(Class<?> clz) {
+        if (clz == int.class || clz == short.class || clz == boolean.class ||
+                clz == float.class || clz == double.class || clz == char.class) {
+            throw new IllegalArgumentException("Enhance method params no support base type. Please change the base type to Object. like int convert to Integer.");
+        }
     }
 
     static {
@@ -77,18 +96,26 @@ public class MethodInvokeUtils {
         pl.importPackage("java.lang");
     }
 
-    public static void main(String[] args) throws NoSuchMethodException, IOException, CannotCompileException, NotFoundException {
-        A a = new A();
-        MethodInvoke invoke = MethodInvokeUtils.enhanceInvoke(a, a.getClass().getMethod("s", String.class));
-        System.out.println(invoke.invoke("ssss"));
-    }
+    public static void main(String[] args) throws NoSuchMethodException, IOException, CannotCompileException, NotFoundException, ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        class A {
+            public String s(String s) {
+                System.out.println(s);
+                return "world";
+            }
 
-    static class A {
-        public String s(String s) {
-            System.out.println(s);
-            return "hello world";
+            public String hello(Integer i) {
+                System.out.println(i);
+                return "hello world";
+            }
         }
 
-        ;
+        A a = new A();
+        MethodInvoke invoke = MethodInvokeUtils.enhanceInvoke(a, a.getClass().getMethod("hello", Integer.class));
+        System.out.println(invoke.invoke(123));
+
+        MethodInvoke s = MethodInvokeUtils.enhanceInvoke(a, a.getClass().getMethod("s", String.class));
+        System.out.println(s.invoke("hello"));
     }
+
+
 }
