0e678ec7670ca455946cc37ac829f7dacfc82011
lzh <867708644@qq.com>
2022-07-26 19:47:07 +0800

    Update commentary

--- game-resource/src/main/java/com/lzh/game/resource/data/cache/ResourceCache.java
+++ game-resource/src/main/java/com/lzh/game/resource/data/cache/ResourceCache.java
@@ -5,6 +5,12 @@ import com.lzh.game.resource.data.ResourceModel;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Resource cache
+ * In local memory or redis?
+ * @param <K> cache key
+ * @param <T> cache data
+ */
 public interface ResourceCache<K extends Serializable, T> {
 
     List<T> findAll();
