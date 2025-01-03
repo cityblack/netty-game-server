e8b3afcd4314805fa48e4998235203e9f4851bfb
lzh <867708644@qq.com>
2025-01-03 16:52:19 +0800

    update

--- game-start/src/main/java/com/lzh/game/start/model/util/reward/RewardType.java
+++ game-start/src/main/java/com/lzh/game/start/model/util/reward/RewardType.java
@@ -1,8 +1,6 @@
 package com.lzh.game.start.model.util.reward;
 
-import com.lzh.game.start.model.core.util.reward.handle.*;
 import com.lzh.game.start.model.util.reward.handle.AndReward;
-import com.lzh.game.start.model.util.reward.handle.CurrencyReward;
 import com.lzh.game.start.model.util.reward.handle.ItemReward;
 
 public enum RewardType {
@@ -21,12 +19,7 @@ public enum RewardType {
         }
     },
 
-    CURRENCY {
-        @Override
-        public CurrencyReward create() {
-            return new CurrencyReward();
-        }
-    }
+
     ;
 
     public abstract <T extends AbstractReward> T create();
